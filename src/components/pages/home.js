1780065ec6ce600e6e2da0af7e7a4170d4535b89
its-nedum <de.magnificent1@gmail.com>
2020-09-12 18:01:37 +0100

    chores(home page design)

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -5,7 +5,14 @@ const Home = () => {
     return (
         <div>
             <div className="first-section mt-5">
-
+                <div className="inner-content container-fluid">
+                    <div className="row">
+                        <div className="col-12 col-sm-8 intro-txt">
+                            <h1>A Better Way <br/>To Exchange Acts Of Kindness</h1>
+                        </div>
+                        <div className="col-sm-4"></div>
+                    </div>
+                </div>
             </div>
         </div>
     )
