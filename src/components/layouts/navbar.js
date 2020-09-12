fdf1a6d5b06eaa5dae2f900c6f8477fc16d54489
its-nedum <de.magnificent1@gmail.com>
2020-09-12 11:27:10 +0100

    features(started navbar implementation)

--- src/components/layouts/navbar.js
+++ src/components/layouts/navbar.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import {NavLink, Link} from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div>
-            <h1>Navbar Section</h1>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <NavLink className="navbar-brand" to="/">Neighborhood Aid</NavLink>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <NavLink className="nav-link" to="#">About</NavLink>
+                        </li>
+                        <li className="nav-item active">
+                            <NavLink className="nav-link" to="#">Stories</NavLink>
+                        </li>
+                        <li className="nav-item active">
+                            <NavLink className="nav-link" to="#">Log In</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link btn btn-outline-primary" to="#">Create a Free Account</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 }
