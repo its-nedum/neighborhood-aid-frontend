72f03b5c4acddb316d7d16e3532c65281d9a4ac7
its-nedum <de.magnificent1@gmail.com>
2020-09-12 12:02:46 +0100

    chores(started footer implementation)

--- src/components/layouts/footer.js
+++ src/components/layouts/footer.js
@@ -3,7 +3,23 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            <h1>Footer Section</h1>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12 col-sm-6">
+                        <h5 className="footer-headings">Neighborhood Aid</h5>
+                    </div>
+                    <div className="col-12 col-sm-6">
+                        <div className="row">
+                            <div className="col-12 col-sm-6">
+                                <h5 className="footer-headings">Quick LInks</h5>
+                            </div>
+                            <div className="col-12 col-sm-6">
+                                <h5 className="footer-headings">Company</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
