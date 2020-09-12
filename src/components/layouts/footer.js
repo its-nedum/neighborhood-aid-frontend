ac764884a52e863878803dcd445d71052b306164
its-nedum <de.magnificent1@gmail.com>
2020-09-12 13:12:59 +0100

    features(completed footer design)

--- src/components/layouts/footer.js
+++ src/components/layouts/footer.js
@@ -5,15 +5,19 @@ import '../../styles/footer.css'
 const Footer = () => {
     return (
         <div>
+            <hr />
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-12 col-sm-7 mb-3">
-                        <h5 className="footer-headings">Neighborhood Aid</h5>
+                        <h5 className="footer-headings footer-logo">Neighborhood Aid</h5>
+                        <p className="footer-text">Technology can be used in many ways, but it's best used to help people.<br/> This can be at the global level, or it can be used to make a difference right outside your door!</p>
+                        <p className="footer-text">Neighborhood aid helps to facilitate exchanged acts of kindness by connecting<br/> people in need to willing volunteers in your (or any) community.</p>
+                        <p className="footer-copyright">&copy; 2020 Neighborhood Aid. All rights reserved | Developed with <span className="text-danger">♥</span> by <a href="https://github.com/its-nedum" target="_blank" rel="noopener noreferrer">Chinedu Emesue</a></p>
                     </div>
                     <div className="col-12 col-sm-5">
                         <div className="row">
                             <div className="col-12 col-sm-6 mb-3">
-                                <h5 className="footer-headings">Quick LInks</h5>
+                                <h5 className="footer-headings">Quick Links</h5>
                                 <div className="quick_links">
                                 <ul className="">
                                     <li><Link to="/">Home</Link></li>
