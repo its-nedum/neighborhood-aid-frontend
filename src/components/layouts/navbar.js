6d1857874b60ce0b2e73f1fa7e31376de308cb91
its-nedum <de.magnificent1@gmail.com>
2020-09-12 11:43:58 +0100

    chores(navbar done for now)

--- src/components/layouts/navbar.js
+++ src/components/layouts/navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import {NavLink, Link} from 'react-router-dom'
+import '../../styles/navbar.css'
 
 const Navbar = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
                 <NavLink className="navbar-brand" to="/">Neighborhood Aid</NavLink>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -12,13 +13,13 @@ const Navbar = () => {
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
+                        <li className="nav-item">
                             <NavLink className="nav-link" to="#">About</NavLink>
                         </li>
-                        <li className="nav-item active">
+                        <li className="nav-item">
                             <NavLink className="nav-link" to="#">Stories</NavLink>
                         </li>
-                        <li className="nav-item active">
+                        <li className="nav-item">
                             <NavLink className="nav-link" to="#">Log In</NavLink>
                         </li>
                         <li className="nav-item">
