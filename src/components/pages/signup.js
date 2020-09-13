d356f466ddd21df96b10a669373ecaed7dee6aa8
its-nedum <de.magnificent1@gmail.com>
2020-09-13 16:46:48 +0100

    features(completed the sign up page)

--- src/components/pages/signup.js
+++ src/components/pages/signup.js
@@ -3,8 +3,46 @@ import React from 'react'
 const Signup = () => {
     return (
         <div>
-            <div className="auth-banner mt-5">
-                <h1>Sign up Page</h1>
+            <div className="container-fluid auth-banner mt-5">
+                <div className="row pl-3">
+                    <div className="col-md-4"></div>
+                    <div className="col-md-4">
+                        <form className="card mt-5 pl-3">
+                            <h1 className="auth-heading">Create Your Account</h1>
+                            <p className="auth-text">Welcome to a better way to exchange acts of kindness</p>
+                            <div className="row input-group">
+                                <div className="col-12 mb-3">
+                                    <label>First Name</label>
+                                    <input type="text" className="form-control" placeholder="First Name" aria-label="firstname" />
+                                </div>
+                            </div>
+                            <div className="row input-group">
+                                <div className="col-12 mb-3">
+                                    <label>Last Name</label>
+                                    <input type="text" className="form-control" placeholder="Last Name" aria-label="lastname" />
+                                </div>
+                            </div>
+                            <div className="row input-group">
+                                <div className="col-12 mb-3">
+                                    <label>Email Address</label>  
+                                    <input type="email" className="form-control" placeholder="Email Address" aria-label="email" />
+                                </div>
+                            </div>
+                            <div className="row input-group">
+                                <div className="col-12 mb-3">
+                                    <label>Government Approved ID</label>
+                                    <input type="file" className="form-control" accept="image/png,image/jpeg,application/pdf" />
+                                </div>
+                            </div>
+                            <div className="row btn-group pr-4">
+                                <div className="col-12 mb-3">
+                                    <button type="button" className="form-control btn btn-primary">Sign Up</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="col-md-4"></div>
+                </div>
             </div>
         </div>
     )
