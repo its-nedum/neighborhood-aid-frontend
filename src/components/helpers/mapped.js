6895ad820e51595507698cedb849aab8eabe7c66
its-nedum <de.magnificent1@gmail.com>
2020-11-09 11:19:49 +0100

    chores(map is giving the desired result)

--- src/components/helpers/mapped.js
+++ src/components/helpers/mapped.js
@@ -1,30 +1,17 @@
-import React from 'react'
-import {GoogleMap, LoadScript, Marker, InfoWindow} from "@react-google-maps/api"
-// import ico from "./icons/man-user.svg"
-// AIzaSyCyvcvBNnBFKP0Xnbb4K-BnMl9f_NXYhDc
-// https://www.airmeet.com/event/47b35460-0a6d-11eb-9aa2-db9f70964def
+import React, {useState, Fragment} from 'react'
+import {GoogleMap, Marker, InfoWindow, useLoadScript} from "@react-google-maps/api"
+
 const containerStyle = {
     width: '100%',
     height: '100vh'
   };
-  
-  const center = {
-    // The logged in user location will be the center
-    lat: 9.0765,
-    lng: 7.3986
-  };
 
 const Mapped = () => {
-    // const [showInfoWindow, setShowInfoWindow] = useState(false)
-    // const [activeMarker, setActiveMarker] = useState({})
-    // const [selectedPlace, setSelectedPlace] = useState({})
+    const { isLoaded } = useLoadScript({ googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY })
 
-    const onMarkerClick = (e) => {
-        console.log({e})
-        // setSelectedPlace(props)
-        // setActiveMarker(marker)
-        // setShowInfoWindow(true)
-    }
+    const [selectedPlace, setSelectedPlace] = useState(null);
+    const [markerMap, setMarkerMap] = useState({});
+    const [infoOpen, setInfoOpen] = useState(false)
 
     const locations = [
         {id: 1, name: 'Keffi', cord:{lat: 8.8471, lng: 7.8776}, mesg:'I love keffi'},
@@ -32,21 +19,72 @@ const Mapped = () => {
         {id: 3, name: 'Kuje', cord:{lat: 8.8764, lng: 7.2437}, mesg:'I love Kuje' },
     ]
 
+    const center = {lat: 9.0765,lng: 7.3986}
+
+    const fitBounds = map => {
+        const bounds = new window.google.maps.LatLngBounds();
+        locations.map(place => {
+            bounds.extend(place.cord);
+            return place.id;
+        })
+        map.fitBounds(bounds)
+    }
+
+    const loadHandler = map => {
+        // fit map bounds to contain all markers
+        fitBounds(map)
+    }
+
+    const markerLoadHandler = (marker, place) => {
+        return setMarkerMap(prevState => {
+            return {...prevState, [place.id]: marker}
+        });
+    }
+
+    const markerClickHandler = (event, place) => {
+        // remember which place was clicked
+        setSelectedPlace(place)
+
+        // 
+        if(infoOpen){
+            setInfoOpen(false)
+        }
+
+        setInfoOpen(true)
+    }
+    
+const showMap = () => {
     return (
-        <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
-            <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-                {locations.map((place) => {
-                    return(
-                        <Marker key={place.id} position={place.cord} onClick={(e) => {onMarkerClick(e)}}>
-                            <InfoWindow id="test" key={place.id} position={center}><h3>{place.name}</h3></InfoWindow>
-                        </Marker>
-                    )
-                })}
-                
-        
+        <Fragment>
+            <GoogleMap 
+                onLoad={loadHandler} 
+                mapContainerStyle={containerStyle} 
+                center={center} zoom={10}    
+                >
+                {locations.map((place) => (
+                    <Marker 
+                        key={place.id} 
+                        position={place.cord}
+                        onLoad={(marker) => markerLoadHandler(marker, place)}
+                        onClick={(event) => markerClickHandler(event, place)} 
+                        />
+                ))}
+
+                {infoOpen && selectedPlace && (
+                    <InfoWindow 
+                        anchor={markerMap[selectedPlace.id]}
+                        onCloseClick={() => setInfoOpen(false)}>
+                        <div>
+                        <h3>{selectedPlace.name}</h3>
+                        <p>{selectedPlace.mesg}</p>
+                        </div>
+                    </InfoWindow>
+                )}
             </GoogleMap>
-        </LoadScript>
+            </Fragment>
     )
+    }
+    return isLoaded ? showMap() : <p>Loading map please wait...</p>
 }
 
 export default Mapped
